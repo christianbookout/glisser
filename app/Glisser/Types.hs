f6cc24d3b571da662cb3368f7e49b3bc4143e41b
Christian Bookout <christianmbookout@gmail.com>
2023-08-03 14:11:10 -0700

    improve performance and add show instance for boards

--- app/Glisser/Types.hs
+++ app/Glisser/Types.hs
@@ -1,19 +1,29 @@
-module Glisser.Types (Team(..), Direction(..), GameObject(..), Board(..)) where
+{-# OPTIONS_GHC -funbox-strict-fields #-}
 
-data Team = A | B | C | D
+module Glisser.Types (Team(..), Direction(..), GameObject(..), Board(..), GameRow(..)) where
+import Data.List (group)
 
-data Direction = DirUp | DirDown | DirLeft | DirRight
+-- | A team to represent a player or game piece.
+data Team = A | B | C | D deriving Eq
 
+-- | A direction to represent movement or facing.
+data Direction = DirUp | DirDown | DirLeft | DirRight deriving Eq
+
+-- | A game object representing a square of the board.
 data GameObject
-    = Piece Team
-    -- ^ Game piece and team it belongs to
-    | Goal Team Direction
-    -- ^ Goal and team who wants to score inside of it, as well as the direction the goal faces
-    | Block Team
-    -- ^ Block piece and team it belongs to
-    | Empty
+    = GLPiece !Team -- ^ Game piece and team it belongs to
+    | GLGoal !Direction !Team 
+    -- ^ Goal and team who wants to score inside of it, as well as the direction
+    -- the goal faces
+    | GLBlock !Team -- ^ Block piece and team it belongs to
+    | GLEmpty
+    deriving Eq
 
-newtype Board = Board [[GameObject]]
+-- | A row of game objects.
+newtype GameRow = GameRow [GameObject]
+
+-- | A board is a 2D array of game objects.
+newtype Board = Board [GameRow]
 
 instance Show Team where
     show A = "A"
@@ -27,5 +37,17 @@ instance Show Direction where
     show DirLeft = "L"
     show DirRight = "R"
 
+instance Show GameObject where
+    show (GLPiece _) = "P"
+    show (GLGoal _ _) = "G"
+    show (GLBlock _) = "B"
+    show GLEmpty = "E"
+
+instance Show GameRow where
+  show (GameRow objs) = concatMap processEmpties $ group objs
+    where
+      processEmpties g@(GLEmpty:_) = show $ length g
+      processEmpties g = concatMap show g
+
 instance Show Board where
-    show (Board board) = ""
+  show (Board board) = concatMap (\(GameRow row) -> show row ++ "\n") board
