734f75c01330437d9eafaf98b3c540084e4e055a
Christian Bookout <christianmbookout@gmail.com>
2023-08-03 15:03:17 -0700

    in prog implementation

--- app/Glisser/Glisser.hs
+++ app/Glisser/Glisser.hs
@@ -1,4 +1,16 @@
-module Glisser.Glisser (GameObject(..)) where
+module Glisser.Glisser (makeMove) where
 import Glisser.Types
 
--- TODO validate game here
+import Control.Monad (guard)
+
+type Glisser a = Maybe a
+
+getPiece :: Board -> (Int, Int) -> Glisser GameObject
+getPiece (Board rows) (x, y) = do
+  guard $ y < length rows
+  let row = rows !! y
+  guard $ x < length row
+  return $ row !! x
+
+makeMove :: Board -> Move -> Glisser Board
+makeMove board (x, y, dir) = return board
